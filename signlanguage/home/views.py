650d330e37ef7107b83407d22b7c976ac5967cdb
Naslaa <naslamaharjan1@gmail.com>
2023-12-17 12:08:39 +0545

    ASL animation
    words and letters

--- signlanguage/home/views.py
+++ signlanguage/home/views.py
@@ -60,98 +60,73 @@ from django.http import StreamingHttpResponse
 from django.utils.decorators import method_decorator
 from django.urls import reverse
 
-
+import os
 from nltk.stem import WordNetLemmatizer
 from nltk import pos_tag
 from django.contrib.staticfiles import finders
 from sklearn.feature_extraction.text import CountVectorizer
 
+def find_video(word):
+    path = f"C:/django/TheSilentVoice-signlanguagerecognition/signlanguage/static/assets/ASL/{word}.mp4"  # Change 'path_to_video_folder' to your video folder path
+    return os.path.isfile(path)
+
+# Function to analyze text using Bag of Words model
+def analyze_text(sentence):
+    # Tokenizing the sentence
+    words = word_tokenize(sentence.lower())
+
+    # Using NLTK's Part-of-Speech tagging
+    tagged = nltk.pos_tag(words)
+
+    # Lemmatizing and filtering words
+    lr = WordNetLemmatizer()
+    filtered_text = []
+    for w, p in zip(words, tagged):
+        if p[1] in ['VBG', 'VBD', 'VBZ', 'VBN', 'NN']:
+            filtered_text.append(lr.lemmatize(w, pos='v'))
+        elif p[1] in ['JJ', 'JJR', 'JJS', 'RBR', 'RBS']:
+            filtered_text.append(lr.lemmatize(w, pos='a'))
+        else:
+            filtered_text.append(lr.lemmatize(w))
+
+    return ' '.join(filtered_text)
 
 def animation_view(request):
-	if request.method == 'POST':
-		text = request.POST.get('sen')
-		#tokenizing the sentence
-		text.lower()
-		#tokenizing the sentence
-		words = word_tokenize(text)
-
-		tagged = nltk.pos_tag(words)
-		tense = {}
-		tense["future"] = len([word for word in tagged if word[1] == "MD"])
-		tense["present"] = len([word for word in tagged if word[1] in ["VBP", "VBZ","VBG"]])
-		tense["past"] = len([word for word in tagged if word[1] in ["VBD", "VBN"]])
-		tense["present_continuous"] = len([word for word in tagged if word[1] in ["VBG"]])
-
-
-
-		#stopwords that will be removed
-		stop_words = set(["mightn't", 're', 'wasn', 'wouldn', 'be', 'has', 'that', 'does', 'shouldn', 'do', "you've",'off', 'for', "didn't", 'm', 'ain', 'haven', "weren't", 'are', "she's", "wasn't", 'its', "haven't", "wouldn't", 'don', 'weren', 's', "you'd", "don't", 'doesn', "hadn't", 'is', 'was', "that'll", "should've", 'a', 'then', 'the', 'mustn', 'i', 'nor', 'as', "it's", "needn't", 'd', 'am', 'have',  'hasn', 'o', "aren't", "you'll", "couldn't", "you're", "mustn't", 'didn', "doesn't", 'll', 'an', 'hadn', 'whom', 'y', "hasn't", 'itself', 'couldn', 'needn', "shan't", 'isn', 'been', 'such', 'shan', "shouldn't", 'aren', 'being', 'were', 'did', 'ma', 't', 'having', 'mightn', 've', "isn't", "won't"])
-
-
-
-		#removing stopwords and applying lemmatizing nlp process to words
-		lr = WordNetLemmatizer()
-		filtered_text = []
-		for w,p in zip(words,tagged):
-			if w not in stop_words:
-				if p[1]=='VBG' or p[1]=='VBD' or p[1]=='VBZ' or p[1]=='VBN' or p[1]=='NN':
-					filtered_text.append(lr.lemmatize(w,pos='v'))
-				elif p[1]=='JJ' or p[1]=='JJR' or p[1]=='JJS'or p[1]=='RBR' or p[1]=='RBS':
-					filtered_text.append(lr.lemmatize(w,pos='a'))
-
-				else:
-					filtered_text.append(lr.lemmatize(w))
-
-
-		#adding the specific word to specify tense
-		words = filtered_text
-		temp=[]
-		for w in words:
-			if w=='I':
-				temp.append('Me')
-			else:
-				temp.append(w)
-		words = temp
-		probable_tense = max(tense,key=tense.get)
-
-		if probable_tense == "past" and tense["past"]>=1:
-			temp = ["Before"]
-			temp = temp + words
-			words = temp
-		elif probable_tense == "future" and tense["future"]>=1:
-			if "Will" not in words:
-					temp = ["Will"]
-					temp = temp + words
-					words = temp
-			else:
-				pass
-		elif probable_tense == "present":
-			if tense["present_continuous"]>=1:
-				temp = ["Now"]
-				temp = temp + words
-				words = temp
-
-
-		filtered_text = []
-		for w in words:
-			path = w + ".mp4"
-			f = finders.find(path)
-			#splitting the word if its animation is not present in database
-			if not f:
-				for c in w:
-					filtered_text.append(c)
-			#otherwise animation of word
-			else:
-				filtered_text.append(w)
-		words = filtered_text;
-		
+    if request.method == 'POST':
+        text = request.POST.get('sen')
         
+        # Analyze the text using the Bag of Words model
+        analyzed_text = analyze_text(text)
 
+        # Create a list with the analyzed text
+        analyzed_text_list = [analyzed_text]
 
-		return render(request,'animation.html',{'words':words,'text':text})
-	else:
+        # Using CountVectorizer to create Bag of Words representation
+        vectorizer = CountVectorizer()
+        X = vectorizer.fit_transform(analyzed_text_list)
 
-		return render(request,'animation.html')
+        # Get the vocabulary
+        vocabulary = vectorizer.get_feature_names_out()
+
+        # List of words from the analyzed text
+        words = vocabulary.tolist()
+
+        # Check for existence of videos in the database
+        filtered_text = []
+        for w in words:
+            # Check if video for word exists in the database
+            if find_video(w):
+                filtered_text.append(w)
+            else:
+                # If video for the word doesn't exist, play videos of individual letters
+                for c in w:
+                    filtered_text.append(c)
+
+        words = filtered_text
+
+        return render(request, 'animation.html', {'words': words, 'text': text})
+    else:
+        return render(request, 'animation.html')
 
 import unicodedata
 
