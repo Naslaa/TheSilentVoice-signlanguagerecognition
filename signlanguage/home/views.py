9a00c6b96309f53c00a0a1a8540750169fddf6d8
Naslaa <naslamaharjan1@gmail.com>
2024-01-25 10:51:30 +0545

    final
    completed project

--- signlanguage/home/views.py
+++ signlanguage/home/views.py
@@ -1,18 +1,16 @@
 from django.shortcuts import render
 from tensorflow.keras.models import load_model
-
-
-import cv2
-from django.http import StreamingHttpResponse
-from django.views.decorators import gzip
 from django.http import JsonResponse
+import cv2
+
+from django.views.decorators import gzip
+
 import nltk
 import cv2
-import time
+
 import speech_recognition as sr
-import pyttsx3
+
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 from nltk.tokenize import word_tokenize
@@ -35,7 +33,7 @@ import cv2
 
 
 # In your Django app's views.py
-from django.shortcuts import render
+
 from django.http import StreamingHttpResponse
 from django.views.decorators import gzip
 import cv2
@@ -51,12 +49,12 @@ import numpy as np
 import base64
 
 
-from django.views.decorators import gzip
+
 from django.views import View
 from django.http import StreamingHttpResponse
 from django.utils.decorators import method_decorator
 from django.urls import reverse
-
+import string
 import os
 from nltk.stem import WordNetLemmatizer
 from nltk import pos_tag
@@ -64,7 +62,7 @@ from django.contrib.staticfiles import finders
 from sklearn.feature_extraction.text import CountVectorizer
 
 def find_video(word):
-    path = f"E:/django/TheSilentVoice-signlanguagerecognition/signlanguage/static/assets/ASL/{word}.mp4"  # Change 'path_to_video_folder' to your video folder path
+    path = f"C:/django/TheSilentVoice-signlanguagerecognition/signlanguage/static/assets/ASL/{word}.mp4"  # Change 'path_to_video_folder' to your video folder path
     return os.path.isfile(path)
 
 # Function to analyze text using Bag of Words model
@@ -76,15 +74,21 @@ def analyze_text(sentence):
     tagged = nltk.pos_tag(words)
 
     # Lemmatizing and filtering words
+    # stop_words = set(nltk.corpus.stopwords.words('english'))
+    stop_words = ['@', '#', "http", ":", "is", "the", "are", "am", "a", "it", "was", "were", "an", ",", ".", "?", "!", ";", "/"]
+  
+
+    # Lemmatizing and filtering words, also removing stop words and punctuation
     lr = WordNetLemmatizer()
     filtered_text = []
-    for w, p in zip(words, tagged):
-        if p[1] in ['VBG', 'VBD', 'VBZ', 'VBN', 'NN']:
-            filtered_text.append(lr.lemmatize(w, pos='v'))
-        elif p[1] in ['JJ', 'JJR', 'JJS', 'RBR', 'RBS']:
-            filtered_text.append(lr.lemmatize(w, pos='a'))
-        else:
-            filtered_text.append(lr.lemmatize(w))
+    for w, p in tagged:
+        if w not in stop_words and w not in string.punctuation:
+            if p in ['VBG', 'VBD', 'VBZ', 'VBN', 'NN']:
+                filtered_text.append(lr.lemmatize(w, pos='v'))
+            elif p in ['JJ', 'JJR', 'JJS', 'RBR', 'RBS']:
+                filtered_text.append(lr.lemmatize(w, pos='a'))
+            else:
+                filtered_text.append(lr.lemmatize(w))
 
     return ' '.join(filtered_text)
 
@@ -92,30 +96,22 @@ def animation_view(request):
     if request.method == 'POST':
         text = request.POST.get('sen')
         
-        # Analyze the text using the Bag of Words model
         analyzed_text = analyze_text(text)
-
-        # Create a list with the analyzed text
         analyzed_text_list = [analyzed_text]
 
-        # Using CountVectorizer to create Bag of Words representation
+    
         vectorizer = CountVectorizer()
         X = vectorizer.fit_transform(analyzed_text_list)
 
-        # Get the vocabulary
         vocabulary = vectorizer.get_feature_names_out()
 
-        # List of words from the analyzed text
         words = vocabulary.tolist()
 
-        # Check for existence of videos in the database
         filtered_text = []
         for w in words:
-            # Check if video for word exists in the database
             if find_video(w):
                 filtered_text.append(w)
             else:
-                # If video for the word doesn't exist, play videos of individual letters
                 for c in w:
                     filtered_text.append(c)
 
@@ -132,13 +128,13 @@ def custom_nepali_tokenizer(text):
     current_token = ''
     
     for char in text:
-        if unicodedata.category(char)[0] in ('L', 'M'):  # Checks if the character is a letter or a combining mark
+        if unicodedata.category(char)[0] in ('L', 'M'):  
             current_token += char
         else:
             if current_token:
                 tokens.append(current_token)
                 current_token = ''
-            # Append non-letter characters as tokens
+        
             tokens.append(char)
 
     if current_token:
@@ -147,20 +143,16 @@ def custom_nepali_tokenizer(text):
     return tokens
 
 def get_video_for_letter(letter):
-    # Replace this function with code to retrieve the video for a Nepali letter from the database
-    # This function should return the path or URL of the video file corresponding to the letter
-    # Example:
-    video_path = f'E:/django/TheSilentVoice-signlanguagerecognition/signlanguage/static/assets/NSL/{letter}.mp4'  # Adjust this path according to your database structure
+  
+    video_path = f'C:/django/TheSilentVoice-signlanguagerecognition/signlanguage/static/assets/NSL/{letter}.mp4'  # Adjust this path according to your database structure
     return video_path
 
 def nanimation_view(request):
     if request.method == 'POST':
-        text = request.POST.get('sen')
-        
-        # Perform necessary Nepali language processing steps here
-        letters = custom_nepali_tokenizer(text)  # Break Nepali text into individual letters
+        text = request.POST.get('sen') 
+    
+        letters = custom_nepali_tokenizer(text) 
 
-        # Extract videos for each letter
         videos = []
         for letter in letters:
             video_path = get_video_for_letter(letter)
@@ -186,7 +178,7 @@ confidence = None
 ngesture_text=""
 nconfidence = None
 
-model = load_model('E:/django/TheSilentVoice-signlanguagerecognition/signlanguage/model/GSASLmodel.h5')  # Replace with the path to your model
+model = load_model('C:/django/TheSilentVoice-signlanguagerecognition/signlanguage/model/20GSASLmodel.h5')  # Replace with the path to your model
 
 # Initialize MediaPipe Hands
 mp_hands = mp.solutions.hands
@@ -199,6 +191,7 @@ def generate_frames():
 
     confidence = None
     gesture_text = ""
+    
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
@@ -209,47 +202,40 @@ def generate_frames():
         results = hands.process(image_rgb)
 
         if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                # Extract hand landmarks
-                h, w, c = frame.shape
-                x_max, y_max, x_min, y_min = 0, 0, w, h
-                for lm in hand_landmarks.landmark:
-                    x, y = int(lm.x * w), int(lm.y * h)
-                    if x > x_max:
-                        x_max = x
-                    if x < x_min:
-                        x_min = x
-                    if y > y_max:
-                        y_max = y
-                    if y < y_min:
-                        y_min = y
+            hand_landmarks = results.multi_hand_landmarks[0]  # Process only the first hand
 
-                # Draw a rectangle around the detected hand region
-                cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
+            # Extract hand landmarks
+            h, w, c = frame.shape
+            x_max, y_max, x_min, y_min = 0, 0, w, h
+            for lm in hand_landmarks.landmark:
+                x, y = int(lm.x * w), int(lm.y * h)
+                if x > x_max:
+                    x_max = x
+                if x < x_min:
+                    x_min = x
+                if y > y_max:
+                    y_max = y
+                if y < y_min:
+                    y_min = y
 
-                # Crop the hand region for prediction
-                cropped_hand = frame[y_min:y_max, x_min:x_max]
+            cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
 
-                # Check if cropped_hand is empty or None
-                if cropped_hand is not None and cropped_hand.size != 0:
-                    cropped_hand_gray = cv2.cvtColor(cropped_hand, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
-                    cropped_hand_gray = cv2.resize(cropped_hand_gray, (100, 100))
-                    cropped_hand_gray = cropped_hand_gray / 255.0
-                    cropped_hand_gray = np.expand_dims(cropped_hand_gray, axis=0)
-                else:
-                    print("Error: The cropped_hand image is empty or None.")
-                    # Handle the error condition appropriately, e.g., by assigning a default value or raising an exception
+            cropped_hand = frame[y_min:y_max, x_min:x_max]
 
+            if cropped_hand is not None and cropped_hand.size != 0:
+                cropped_hand_gray = cv2.cvtColor(cropped_hand, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
+                cropped_hand_gray = cv2.resize(cropped_hand_gray, (100, 100))
+                cropped_hand_gray = cropped_hand_gray / 255.0
+                cropped_hand_gray = np.expand_dims(cropped_hand_gray, axis=0)
+            else:
+                print("Error: The cropped_hand image is empty or None.")
 
-                # Make a prediction with the CNN model when space is pressed
-                # if cv2.waitKey(1) & 0xFF == ord(' '):
-                prediction = model.predict(cropped_hand_gray)
-                predicted_class = np.argmax(prediction)
-                confidence = prediction[0][predicted_class]
+            prediction = model.predict(cropped_hand_gray)
+            predicted_class = np.argmax(prediction)
+            confidence = prediction[0][predicted_class]
 
-                    # Get the predicted gesture label
-                labels = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "G", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "del", "nothing", "space"]
-                gesture_text = labels[predicted_class]
+            labels = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "G", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "del", "nothing", "space"]
+            gesture_text = labels[predicted_class]
 
         if gesture_text is not None and confidence is not None:
             cv2.putText(frame, f'{gesture_text} ({confidence:.2f})',
@@ -273,7 +259,7 @@ from PIL import Image, ImageDraw, ImageFont
 font_path = 'c:/Windows/Fonts/kokila.ttf'
 
 # font_path='C:/Users/Administrator/AppData/Local/Microsoft/Windows/Fonts/kokila.ttf'
-nmodel = load_model('E:/django/TheSilentVoice-signlanguagerecognition/signlanguage/model/GSNSLmodel.h5')  # Replace with the path to your model
+nmodel = load_model('C:/django/TheSilentVoice-signlanguagerecognition/signlanguage/model/d7NSLmodel.h5')  # Replace with the path to your model
 
 # Initialize MediaPipe Hands
 mp_hands = mp.solutions.hands
@@ -294,54 +280,48 @@ def ngenerate_frames():
         results = hands.process(image_rgb)
 
         if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                # Extract hand landmarks
-                h, w, c = frame.shape
-                x_max, y_max, x_min, y_min = 0, 0, w, h
-                for lm in hand_landmarks.landmark:
-                    x, y = int(lm.x * w), int(lm.y * h)
-                    if x > x_max:
-                        x_max = x
-                    if x < x_min:
-                        x_min = x
-                    if y > y_max:
-                        y_max = y
-                    if y < y_min:
-                        y_min = y
+            hand_landmarks = results.multi_hand_landmarks[0]  # Process only the first hand
 
-                # Draw a rectangle around the detected hand region
-                cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
+            # Extract hand landmarks
+            h, w, c = frame.shape
+            x_max, y_max, x_min, y_min = 0, 0, w, h
+            for lm in hand_landmarks.landmark:
+                x, y = int(lm.x * w), int(lm.y * h)
+                if x > x_max:
+                    x_max = x
+                if x < x_min:
+                    x_min = x
+                if y > y_max:
+                    y_max = y
+                if y < y_min:
+                    y_min = y
 
-# Crop the hand region for prediction
-                cropped_hand = frame[y_min:y_max, x_min:x_max]
+            # Draw a rectangle around the detected hand region
+            cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
 
-                # Check if cropped_hand is empty or None
-                if cropped_hand is not None and cropped_hand.size != 0:
-                    cropped_hand_gray = cv2.cvtColor(cropped_hand, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
-                    cropped_hand_gray = cv2.resize(cropped_hand_gray, (100, 100))
-                    cropped_hand_gray = cropped_hand_gray / 255.0
-                    cropped_hand_gray = np.expand_dims(cropped_hand_gray, axis=0)
-                else:
-                    print("Error: The cropped_hand image is empty or None.")
+            cropped_hand = frame[y_min:y_max, x_min:x_max]
 
-                # Make a prediction with the CNN model when space is pressed
-                # if cv2.waitKey(1) & 0xFF == ord(' '):
-                prediction = nmodel.predict(cropped_hand_gray)
-                predicted_class = np.argmax(prediction)
-                nconfidence = prediction[0][predicted_class]
+            if cropped_hand is not None and cropped_hand.size != 0:
+                cropped_hand_gray = cv2.cvtColor(cropped_hand, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
+                cropped_hand_gray = cv2.resize(cropped_hand_gray, (100, 100))
+                cropped_hand_gray = cropped_hand_gray / 255.0
+                cropped_hand_gray = np.expand_dims(cropped_hand_gray, axis=0)
+            else:
+                print("Error: The cropped_hand image is empty or None.")
 
-                    # Get the predicted gesture label
-                labels = ["क","क्ष", "ख", "ग", "घ", "ङ", "च", "छ", "ज","ज्ञ", "झ", "ञ", "ट", "ठ", "ड", "ढ", "ण", "त", "त्र", "थ", "द", "ध", "न", "प", "फ", "ब", "भ", "म", "य", "र", "ल", "व", "श", "ष", "स", "ह"]
-                ngesture_text = labels[predicted_class]
+            prediction = nmodel.predict(cropped_hand_gray)
+            predicted_class = np.argmax(prediction)
+            nconfidence = prediction[0][predicted_class]
 
-        # if gesture_text is not None and confidence is not None:
-        #     cv2.putText(frame, f'{gesture_text} ({confidence:.2f})',
-        #                 (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            labels = ["क", "क्ष", "ख", "ग", "घ", "ङ", "च", "छ", "ज", "ज्ञ", "झ", "ञ", "ट", "ठ", "ड", "ढ", "ण", "त", "त्र", "थ", "द", "ध", "न", "प", "फ", "ब", "भ", "म", "य", "र", "ल", "व", "श", "ष", "स", "ह"]
+            ngesture_text = labels[predicted_class]
+
+
        
         if ngesture_text is not None and nconfidence is not None:
             text_to_display = f'{ngesture_text} ({nconfidence:.2f})'
         else:
-            text_to_display = "No prediction available"  # Placeholder text if either is None
+            text_to_display = "No prediction available" 
         pil_image = Image.fromarray(frame)
         draw = ImageDraw.Draw(pil_image)
         text_font = ImageFont.truetype(font_path, 30)
@@ -367,23 +347,18 @@ def ncamera_view(request):
 
 def perform_prediction(request):
     global gesture_text
-    # Add code to perform the prediction using your model
-    # Example placeholder response (modify as per your actual prediction logic)
+  
     predicted_character = gesture_text
-    predicted_word = "Apple"
-    predicted_sentence = "This is a sample sentence."
+
 
     # Return the predicted data as a JSON response
     return JsonResponse({
         'character': predicted_character,
-        'word': predicted_word,
-        'sentence': predicted_sentence,
     },content_type='application/json')
 
 def nperform_prediction(request):
     global ngesture_text
-    # Add code to perform the prediction using your model
-    # Example placeholder response (modify as per your actual prediction logic)
+
     predicted_character = ngesture_text
 
     # Return the predicted data as a JSON response
